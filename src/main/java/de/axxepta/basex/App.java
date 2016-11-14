186fb4b8cb9e053cf829b7a71cb4f204d37700b3
mgaerber <gaerber@axxepta.de>
2016-11-14 23:59:26 +0100

    add spark handler

--- src/main/java/de/axxepta/basex/App.java
+++ src/main/java/de/axxepta/basex/App.java
@@ -1,13 +1,46 @@
 package de.axxepta.basex;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+
+import org.basex.core.StaticOptions;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.Close;
+import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.DropDB;
+import org.basex.core.cmd.Open;
+import org.basex.core.cmd.XQuery;
+import org.basex.query.QueryException;
+
+public class App {
+
+    private static String dbName = "stress-test";
+
+    public static void main(String[] args) {
+
+        //get("/hello", (req, res) -> "Hello World");
+
+        get("/collection/:name", (request, response) -> {
+
+            String name = request.params(":name");
+            Context ctx = new Context();
+            String dbpath =  ctx.soptions.get(StaticOptions.DBPATH);
+            String userDbPath = dbpath + "\\" + name;
+            //set new DBPATH
+            ctx.soptions.put(StaticOptions.DBPATH, userDbPath);
+
+            String res = new XQuery(String.format("collection('%s')/container", App.dbName + "/" + name + ".xml")).execute(ctx);
+
+            response.type("text/xml");
+
+            return res;
+        });
+
+        exception(QueryException.class, (exception, request, response) -> {
+            // Handle the exception here
+
+            //return exception.getMessage();
+            System.err.println(exception.getMessage());
+        });
     }
 }
